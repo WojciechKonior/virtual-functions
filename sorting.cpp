8e0f87d34a6ca93c5c9d26fa40adafb0c1eaa713
Wojciech Konior <konior.wojciech@gmail.com>
2023-08-03 18:41:27 +0200

    8th commit

---
+++ sorting.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <deque>
+#include <vector>
+
+using namespace std;
+
+int s = 10;
+int arr[] = {10, 2, 3, 1, 9, 7, 8, 4, 6, 5};
+deque<int> deq = {10, 2, 3, 1, 9, 7, 8, 4, 6, 5};
+vector<int> unsorted = {10, 2, 3, 1, 9, 7, 8, 4, 6, 5};
+vector<int> sorted;
+
+int main(){
+
+    for(int i = 0; i<s; i++)
+    {
+
+    }
+
+    for(int i = 0; i<s; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
